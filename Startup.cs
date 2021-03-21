b832560b31021335ee03a9991ddf6c8c3f1b9d6a
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2021-03-21 22:52:10 +0100

    Docker deployment configurations

--- Startup.cs
+++ Startup.cs
@@ -38,16 +38,9 @@ namespace pwr_msi
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
 
             app.UseRouting();
 
