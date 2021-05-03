8396114bec19b7147008bf2338c83f33cd1e2d7f
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2021-05-03 13:22:07 +0200

    Move repository port to avoid clashes with app

--- scripts/docker-build-push.sh
+++ scripts/docker-build-push.sh
@@ -2,12 +2,12 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 cd ..
-echo "Ensure the repository is at localhost:5000 (try ssh -L 5000:localhost:5000 destination)"
+echo "Ensure the repository is at localhost:5003 (try ssh -L 5003:localhost:5003 destination)"
 read confirm
 docker-compose -f docker-compose.prod.yml build
-docker tag pwr-msi_nginx:latest localhost:5000/pwr-msi_nginx:latest
-docker tag pwr-msi_backend:latest localhost:5000/pwr-msi_backend:latest
-docker tag pwr-msi_msipay:latest localhost:5000/pwr-msi_msipay:latest
-docker push localhost:5000/pwr-msi_nginx:latest
-docker push localhost:5000/pwr-msi_backend:latest
-docker push localhost:5000/pwr-msi_msipay:latest
+docker tag pwr-msi_nginx:latest localhost:5003/pwr-msi_nginx:latest
+docker tag pwr-msi_backend:latest localhost:5003/pwr-msi_backend:latest
+docker tag pwr-msi_msipay:latest localhost:5003/pwr-msi_msipay:latest
+docker push localhost:5003/pwr-msi_nginx:latest
+docker push localhost:5003/pwr-msi_backend:latest
+docker push localhost:5003/pwr-msi_msipay:latest
--- scripts/docker-pull-run-prod.sh
+++ scripts/docker-pull-run-prod.sh
@@ -2,10 +2,10 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 cd ..
-docker pull localhost:5000/pwr-msi_nginx:latest
-docker pull localhost:5000/pwr-msi_backend:latest
-docker pull localhost:5000/pwr-msi_msipay:latest
-docker tag localhost:5000/pwr-msi_nginx:latest pwr-msi_nginx:latest
-docker tag localhost:5000/pwr-msi_backend:latest pwr-msi_backend:latest
-docker tag localhost:5000/pwr-msi_msipay:latest pwr-msi_msipay:latest
+docker pull localhost:5003/pwr-msi_nginx:latest
+docker pull localhost:5003/pwr-msi_backend:latest
+docker pull localhost:5003/pwr-msi_msipay:latest
+docker tag localhost:5003/pwr-msi_nginx:latest pwr-msi_nginx:latest
+docker tag localhost:5003/pwr-msi_backend:latest pwr-msi_backend:latest
+docker tag localhost:5003/pwr-msi_msipay:latest pwr-msi_msipay:latest
 docker-compose --env-file .env.prod -f docker-compose.prod.yml up -d
