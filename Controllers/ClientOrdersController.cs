433af5159fd6598af7ae64e7e10ae74ecb5b9861
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2021-05-26 21:57:15 +0200

    Improve and clean up client orders controller (#18)

--- Controllers/ClientOrdersController.cs
+++ Controllers/ClientOrdersController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -18,57 +19,57 @@ namespace pwr_msi.Controllers {
     public class ClientOrdersController : MsiControllerBase {
         private readonly MsiDbContext _dbContext;
         private readonly PaymentService _paymentService;
+        private readonly OrderDetailsService _orderDetailsService;
         private readonly OrderTaskService _orderTaskService;
-        
-        public ClientOrdersController(MsiDbContext dbContext, PaymentService paymentService, OrderTaskService orderTaskService) {
+
+        public ClientOrdersController(MsiDbContext dbContext, PaymentService paymentService,
+            OrderDetailsService orderDetailsService,
+            OrderTaskService orderTaskService) {
             _dbContext = dbContext;
             _paymentService = paymentService;
+            _orderDetailsService = orderDetailsService;
             _orderTaskService = orderTaskService;
         }
-        
+
+        private IQueryable<Order> OrderBasicQuery() {
+            return _dbContext.Orders.Include(o => o.Customer).Include(o => o.Restaurant).Include(o => o.Address);
+        }
+
         [Route(template: "")]
         public async Task<ActionResult<List<OrderBasicDto>>> AllOrders() {
-            var query = _dbContext.Orders.Where(o => o.CustomerId==MsiUserId); 
+            var query = OrderBasicQuery().Where(o => o.CustomerId == MsiUserId);
             var oList = await query.ToListAsync();
             return oList.Select(o => o.AsBasicDto()).ToList();
         }
-        
+
         [Route(template: "")]
         public async Task<ActionResult<List<OrderBasicDto>>> FilteredOrders([FromQuery] string status) {
-            OrderStatus parsedStatus;
-            Enum.TryParse(status.ToUpper(), out parsedStatus);
-            var query = _dbContext.Orders.Where(o => o.CustomerId==MsiUserId && 
-                                                     o.Status.Equals(parsedStatus)); 
+            if (!Enum.TryParse(status.ToUpper(), out OrderStatus parsedStatus)) return BadRequest();
+            var query = OrderBasicQuery().Where(
+                o => o.CustomerId == MsiUserId &&
+                     o.Status == parsedStatus);
             var oList = await query.ToListAsync();
             return oList.Select(o => o.AsBasicDto()).ToList();
         }
+
         [Route(template: "active/")]
         public async Task<ActionResult<List<OrderBasicDto>>> ActiveOrders() {
-            OrderStatus parsedStatus;
-            var query = _dbContext.Orders.Where(o => o.CustomerId==MsiUserId && 
-                                                     !(o.Status.Equals(OrderStatus.REJECTED) ||
-                                                      o.Status.Equals(OrderStatus.CANCELLED) ||
-                                                      o.Status.Equals(OrderStatus.COMPLETED))); 
+            var query = OrderBasicQuery().Where(o => o.CustomerId == MsiUserId)
+                .Where(o =>
+                    o.Status != OrderStatus.REJECTED &&
+                    o.Status != OrderStatus.CANCELLED &&
+                    o.Status != OrderStatus.COMPLETED);
             var oList = await query.ToListAsync();
             return oList.Select(o => o.AsBasicDto()).ToList();
         }
-        
+
         [Route(template: "{id}/")]
         public async Task<ActionResult<OrderDto>> GetOrder([FromRoute] int id) {
-            Order order = await _dbContext.Orders.FindAsync(id);
-            var query =  _dbContext.OrderItems.Where(oi => oi.OrderId==id && oi.Order.CustomerId==MsiUserId);
-            List<OrderItemCustomization> options = new List<OrderItemCustomization>();
-            var oiList = await query.ToListAsync();
-            foreach (var item in oiList) {
-                var option = _dbContext.OrderItemCustomizations.Where(oic => oic.OrderItemId == item.OrderItemId);
-                var oicList = await option.ToListAsync();
-                foreach (var optionItem in oicList) {
-                    options.Add(optionItem);
-                }
-            }
-            return order.AsDto(options, oiList);
+            OrderDto orderDto = await _orderDetailsService.GetOrderById(id);
+            if (orderDto == null || orderDto.Customer.UserId != MsiUserId) return NotFound();
+            return orderDto;
         }
-        
+
         [Route(template: "{id}/")]
         [HttpDelete]
         public async Task<ActionResult<OrderBasicDto>> CancelOrder([FromRoute] int id) {
@@ -76,62 +77,37 @@ namespace pwr_msi.Controllers {
             if (order == null) return NotFound();
             if (order.Status == OrderStatus.PAID || order.Status == OrderStatus.CREATED) {
                 await _orderTaskService.TryCompleteTask(order, OrderTaskType.CANCEL, MsiUser);
-                await  _paymentService.ReturnOrderPayment(order);
+                await _paymentService.ReturnOrderPayment(order);
                 order.Updated = new ZonedDateTime();
+                await _dbContext.SaveChangesAsync();
+                return order.AsBasicDto();
             }
 
-            await _dbContext.SaveChangesAsync();
-            return order.AsBasicDto();
+            return BadRequest();
         }
-        
+
         [Route(template: "")]
         [HttpPost]
-        public async Task<ActionResult<OrderBasicDto>> CreateOrder([FromBody] OrderCreatingDto ocDto) {
-            Order order = new Order(){
-                DeliveryNotes = ocDto.DeliveryNotes,
-                RestaurantId = ocDto.RestaurantId,
-                AddressId = ocDto.AddressId,
-                CustomerId = ocDto.CustomerId,
-                Created = new ZonedDateTime()};
+        public async Task<ActionResult<OrderDto>> CreateOrder([FromBody] OrderDto orderDto) {
+            Debug.Assert(MsiUserId != null, nameof(MsiUserId) + " != null");
+            var order = await orderDto.AsNewOrder(MsiUserId.Value, _dbContext);
+
             await _dbContext.Orders.AddAsync(order);
-            decimal totalPrice = 0;
-            foreach (var oi in ocDto.Items) {
-                OrderItem orderItem = new OrderItem() {OrderId = order.OrderId, MenuItemId = oi.MenuItemId};
-                await _dbContext.OrderItems.AddAsync(orderItem);
-                totalPrice += oi.TotalPrice;
-            }
-            foreach (var opi in ocDto.OptionItems) {
-                OrderItemCustomization orderOption = new OrderItemCustomization() {OrderItemId = opi.OrderItemId,
-                    MenuItemOptionItemId = opi.OrderItemCustomizationId};
-                await _dbContext.OrderItemCustomizations.AddAsync(orderOption);
-                var optionItem = await _dbContext.MenuItemOptionItems.FindAsync(orderOption.MenuItemOptionItemId);
-                totalPrice += optionItem.Price;
-            }
-            order.TotalPrice = totalPrice;
             await _orderTaskService.TryCompleteTask(order, OrderTaskType.CREATE, MsiUser);
-            await _paymentService.CreatePaymentForOrder(order);
             await _dbContext.SaveChangesAsync();
-            return order.AsBasicDto();
+            return await _orderDetailsService.GetOrderById(order.OrderId);
         }
 
-        [Route(template: "{id}/")]
-        [HttpPut]
-        public async Task<ActionResult<OrderDto>> PayForOrder([FromRoute] int id) {
-            var order = await _dbContext.Orders.FindAsync(id);
-            var query =  _dbContext.OrderItems.Where(oi => oi.OrderId==id && oi.Order.CustomerId==MsiUserId);
-            List<OrderItemCustomization> options = new List<OrderItemCustomization>();
-            var oiList = await query.ToListAsync();
-            foreach (var item in oiList) {
-                var option = _dbContext.OrderItemCustomizations.Where(oic => oic.OrderItemId == item.OrderItemId);
-                var oicList = await option.ToListAsync();
-                foreach (var optionItem in oicList) {
-                    options.Add(optionItem);
-                }
+        [Route(template: "{id}/pay/")]
+        [HttpPost]
+        public async Task<ActionResult<PaymentGroupInfo>> PayForOrder([FromRoute] int id, [FromQuery] bool useBalance) {
+            var order = await _dbContext.Orders.Include(o => o.Customer)
+                .Where(o => o.OrderId == id && o.CustomerId == MsiUserId).FirstOrDefaultAsync();
+            if (order == null) {
+                return NotFound();
             }
-            var payment = await _dbContext.Payments.Where(p => p.OrderId == id).FirstAsync();
-            await _paymentService.MakePayment(payment);
-            await _orderTaskService.TryMarkAsPaid(id);
-            return order.AsDto(options, oiList);
+
+            return await _paymentService.CreatePaymentForOrder(order, useBalance);
         }
     }
 }
