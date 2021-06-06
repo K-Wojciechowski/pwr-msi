5e785e5bc50a1249fbbf430596edc0b7a6eb97be
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2021-06-07 01:33:44 +0200

    Bug fixes and refactoring in delivery controller (Fix #16)

--- Controllers/DeliveryController.cs
+++ Controllers/DeliveryController.cs
@@ -1,4 +1,4 @@
-using System;
+#nullable enable
 using System.Collections.Generic;
 using GeoCoordinatePortable;
 using System.Linq;
@@ -18,134 +18,127 @@ namespace pwr_msi.Controllers {
     public class DeliveryController : MsiControllerBase {
         private readonly MsiDbContext _dbContext;
         private readonly OrderTaskService _orderTaskService;
-        
-        public DeliveryController(MsiDbContext dbContext, OrderTaskService orderTaskService) {
+        private readonly OrderDetailsService _orderDetailsService;
+
+        public DeliveryController(MsiDbContext dbContext, OrderTaskService orderTaskService, OrderDetailsService orderDetailsService) {
             _dbContext = dbContext;
             _orderTaskService = orderTaskService;
+            _orderDetailsService = orderDetailsService;
         }
-        private async Task<List<Order>> GetCloseOrders(IQueryable<Order> query, GeoCoordinate userLoc, int range) {
+
+        private async Task<List<Order>> GetNearbyOrders(IQueryable<Order> query, GeoCoordinate userLoc, int range) {
             var oList = await query.ToListAsync();
+            var nearby = new List<Order>();
             foreach (var o in oList) {
                 var rAddress = new GeoCoordinate(o.Restaurant.Address.Latitude, o.Restaurant.Address.Longitude);
-                if (rAddress.GetDistanceTo(userLoc) > range) {
-                    oList.Remove(o);
+                if (rAddress.GetDistanceTo(userLoc) <= range) {
+                    nearby.Add(o);
                 }
             }
-            return oList;
+
+            return nearby;
         }
 
         [Route(template: "active/")]
-        public async Task<ActionResult<List<OrderBasicDto>>> AllActiveOrders() {
-            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId==MsiUserId
-                                                     && (o.Status.Equals(OrderStatus.PREPARED) 
-                                                         || o.Status.Equals(OrderStatus.ACCEPTED)));
+        public async Task<ActionResult<List<OrderBasicDto>>> AllActiveOrders([FromQuery] int restaurant = -1) {
+            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId == MsiUserId)
+                .Where(o =>
+                    o.Status == OrderStatus.PREPARED || o.Status == OrderStatus.ACCEPTED);
+            if (restaurant > 0) {
+                query = query.Where(o => o.RestaurantId == restaurant);
+            }
+
             var oList = await query.ToListAsync();
             return oList.Select(o => o.AsBasicDto()).ToList();
         }
-        
-        [Route(template: "active/")]
-        public async Task<ActionResult<List<OrderBasicDto>>> FilteredActiveOrders([FromQuery] string restaurant) {
-            var likeQuery = $"%{restaurant}%";
-            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId==MsiUserId
-                                                     && (o.Status.Equals(OrderStatus.PREPARED) 
-                                                         || o.Status.Equals(OrderStatus.ACCEPTED))
-                                                     && EF.Functions.ILike(o.Restaurant.Name, likeQuery));
-            var oList = await query.ToListAsync();
-            return oList.Select(o => o.AsBasicDto()).ToList();
-        }
-        
+
         [Route(template: "waiting/")]
-        public async Task<ActionResult<List<OrderBasicDto>>> AllWaitingOrders([FromQuery] float lat,[FromQuery] float lng, [FromQuery] int range) {
-            var restaurants = _dbContext.RestaurantUsers.Where(ru => ru.UserId == MsiUserId);
-            var query = _dbContext.Orders.Where(o => o.DeliveryPerson==null 
-                                                     && restaurants.Any(r => r.RestaurantId == o.RestaurantId)
-                                                     && (o.Status.Equals(OrderStatus.PREPARED) 
-                                                         || o.Status.Equals(OrderStatus.ACCEPTED)));
+        public async Task<ActionResult<List<OrderBasicDto>>> AllWaitingOrders([FromQuery] float lat,
+            [FromQuery] float lng, [FromQuery] int range, [FromQuery] int restaurantId) {
+            var restaurants = await _dbContext.RestaurantUsers
+                .Where(ru => ru.UserId == MsiUserId && ru.CanDeliverOrders)
+                .Select(ru => ru.RestaurantId).ToListAsync();
+            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId == null)
+                .Where(o => restaurants.Contains(o.RestaurantId))
+                .Where(o =>
+                    o.Status == OrderStatus.PREPARED || o.Status == OrderStatus.ACCEPTED);
 
-            var userAddress = new GeoCoordinate(lat, lng);
-            var oList = await GetCloseOrders(query, userAddress, range);
-            return oList.Select(o => o.AsBasicDto()).ToList();
+            List<Order> orders;
+            if (range == 0) {
+                orders = await query.ToListAsync();
+            } else {
+                var enrichedQuery = query.Include(o => o.Restaurant).Include("Restaurant.Address");
+                var userAddress = new GeoCoordinate(lat, lng);
+                orders = await GetNearbyOrders(enrichedQuery, userAddress, range);
+            }
+
+            return orders.Select(o => o.AsBasicDto()).ToList();
         }
-        
-        [Route(template: "waiting/")]
-        public async Task<ActionResult<List<OrderBasicDto>>> FilteredWaitingOrders([FromQuery] string restaurant, 
-            [FromQuery] float lat,[FromQuery] float lng, [FromQuery] int range) {
-            var likeQuery = $"%{restaurant}%";
-            var restaurants = _dbContext.RestaurantUsers.Where(ru => ru.UserId == MsiUserId);
-            var query = _dbContext.Orders.Where(o => o.DeliveryPerson==null 
-                                                     && restaurants.Any(r => r.RestaurantId == o.RestaurantId)
-                                                     && (o.Status.Equals(OrderStatus.PREPARED) 
-                                                         || o.Status.Equals(OrderStatus.ACCEPTED))
-                                                     && EF.Functions.ILike(o.Restaurant.Name, likeQuery));
-            var userAddress = new GeoCoordinate(lat, lng);
-            var oList = await GetCloseOrders(query, userAddress, range);
-            return oList.Select(o => o.AsBasicDto()).ToList();
-        }
-        
-        [Route(template: "completed/")]
-        public async Task<ActionResult<List<OrderBasicDto>>> AllCompletedOrders() {
-            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId==MsiUserId 
-                                                     && (o.Status.Equals(OrderStatus.COMPLETED) 
-                                                         || o.Status.Equals(OrderStatus.DELIVERED)));
+
+        [Route(template: "history/")]
+        public async Task<ActionResult<List<OrderBasicDto>>> AllCompletedOrders([FromQuery] int restaurant = -1) {
+            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId == MsiUserId);
+            if (restaurant > 0) {
+                query = query.Where(o => o.RestaurantId == restaurant);
+            }
+
             var oList = await query.ToListAsync();
             return oList.Select(o => o.AsBasicDto()).ToList();
         }
 
-        [Route(template: "completed/")] 
-        public async Task<ActionResult<List<OrderBasicDto>>> FilteredCompletedOrders([FromQuery] string restaurant) {
-            var likeQuery = $"%{restaurant}%";
-            var query = _dbContext.Orders.Where(o => o.DeliveryPersonId==MsiUserId
-                                                     && (o.Status.Equals(OrderStatus.COMPLETED) 
-                                                         || o.Status.Equals(OrderStatus.DELIVERED))
-                                                     && EF.Functions.ILike(o.Restaurant.Name, likeQuery));
-            var oList = await query.ToListAsync();
-            return oList.Select(o => o.AsBasicDto()).ToList();
-        }
-
-        [Route(template: "active/{id}/complete")]
+        [Route(template: "active/{id}/complete/")]
         [HttpPost]
         public async Task<ActionResult<OrderBasicDto>> MarkAsDelivered([FromRoute] int id) {
-            var order = await _dbContext.Orders.FindAsync(id);
-            if (order == null || order.DeliveryPersonId!=MsiUserId 
-                              || !(order.Status.Equals(OrderStatus.PREPARED) 
-                                   || order.Status.Equals(OrderStatus.ACCEPTED))) return NotFound();
+            var orderQuery = _dbContext.Orders
+                .Where(o => o.OrderId == id)
+                .Where(o => o.DeliveryPersonId == MsiUserId)
+                .Where(o => o.Status == OrderStatus.PREPARED || o.Status == OrderStatus.ACCEPTED);
+            var order = await orderQuery.FirstOrDefaultAsync();
+
+            if (order == null) return NotFound();
             await _orderTaskService.TryCompleteTask(order, OrderTaskType.DELIVER, MsiUser);
             return order.AsBasicDto();
         }
-        
-        [Route(template: "waiting/{id}/assign")]
+
+        [Route(template: "waiting/{id}/assign/")]
         [HttpPost]
-        public async Task<ActionResult<OrderBasicDto>> AssignYourself([FromRoute] int id) {
-            var order = await _dbContext.Orders.FindAsync(id);
-            if (order == null || order.DeliveryPerson!=null
-                              || !(order.Status.Equals(OrderStatus.PREPARED) 
-                                   || order.Status.Equals(OrderStatus.ACCEPTED))) return NotFound();
+        public async Task<ActionResult<OrderBasicDto>> AssignToSelf([FromRoute] int id) {
+            var restaurants = await _dbContext.RestaurantUsers
+                .Where(ru => ru.UserId == MsiUserId && ru.CanDeliverOrders)
+                .Select(ru => ru.RestaurantId).ToListAsync();
+
+            var orderQuery = _dbContext.Orders
+                .Where(o => o.OrderId == id)
+                .Where(o => o.DeliveryPersonId == null)
+                .Where(o => restaurants.Contains(o.RestaurantId))
+                .Where(o => o.Status == OrderStatus.PREPARED || o.Status == OrderStatus.ACCEPTED);
+            var order = await orderQuery.FirstOrDefaultAsync();
+
+            if (order == null) return NotFound();
             order.DeliveryPersonId = MsiUserId;
-            order.DeliveryPerson = MsiUser;
+
+            // TODO register/assign order task
+
             await _dbContext.SaveChangesAsync();
             return order.AsBasicDto();
         }
 
         [Route("order/{id}/")]
-        public async Task<ActionResult<OrderDetailsDto>> OrderDetails([FromRoute] int id) {
-            var restaurants = _dbContext.RestaurantUsers.Where(ru => ru.UserId == MsiUserId);
-            var order = await _dbContext.Orders.Where(o => (o.DeliveryPerson==null 
-                                                                    && restaurants.Any(r => r.RestaurantId == o.RestaurantId) 
-                                                                    && (o.Status.Equals(OrderStatus.PREPARED) 
-                                                                    || o.Status.Equals(OrderStatus.ACCEPTED))
-                                                                || o.DeliveryPersonId==MsiUserId )).FirstOrDefaultAsync();
-            
-            var queryItems = _dbContext.OrderItems.Where(oi => oi.OrderId == id);
-            ICollection<OrderItem> items = await queryItems.ToListAsync(); 
-            ICollection<OrderItemCustomization> options = new List<OrderItemCustomization>();
-            foreach (var item in items) {
-                var queryOptions =_dbContext.OrderItemCustomizations.Where(oi=> oi.OrderItemId==item.OrderItemId);
-                foreach (var option in queryOptions) {
-                    options.Add(option);
-                    
-                }
-            }
-            return order == null ? NotFound() : order.AsDetailedDto(items, options);
+        public async Task<ActionResult<OrderDto?>> OrderDetails([FromRoute] int id) {
+            var restaurants = await _dbContext.RestaurantUsers
+                .Where(ru => ru.UserId == MsiUserId && ru.CanDeliverOrders)
+                .Select(ru => ru.RestaurantId).ToListAsync();
+
+            var orderQuery = _dbContext.Orders
+                .Where(o => o.OrderId == id)
+                .Where(o => o.DeliveryPersonId == null || o.DeliveryPersonId == MsiUserId)
+                .Where(o => restaurants.Contains(o.RestaurantId))
+                .Where(o => o.DeliveryPersonId == MsiUserId || o.Status == OrderStatus.PREPARED || o.Status == OrderStatus.ACCEPTED);
+
+            var order = await orderQuery.FirstOrDefaultAsync();
+
+            if (order == null) return NotFound();
+            return await _orderDetailsService.GetOrderById(order.OrderId, includeDeliveryPerson: true);
         }
     }
 }
